6d4b1af71518c05ed381f3349e35ca1d2545ecce
Jason <j.vanschaik@tcrmbo.nl>
2023-03-29 09:52:36 +0200

    opdracht 5.1

---
+++ app.js
@@ -0,0 +1,6 @@
+document.getElementById('titel').style.color = 'blue';
+document.getElementById('titel').style.fontWeight = 'bold';
+
+
+//haal het element P op
+document.getElementById("para").innerText = "Youri is een uitslover";
